c123688e2b138ef47177534202ac18e7b59ecf8e
latitud38 <jonathan.mellado@mayor.cl>
2025-07-07 15:27:02 -0400

    Add files via upload

---
+++ qgis2web_2025_07_07-15_17_24_717216/styles/BarrioEstacin_18_style.js
@@ -0,0 +1,43 @@
+var size = 0;
+var placement = 'point';
+
+var style_BarrioEstacin_18 = function(feature, resolution){
+    var context = {
+        feature: feature,
+        variables: {}
+    };
+    
+    var labelText = ""; 
+    var value = feature.get("");
+    var labelFont = "13.0px \'Open Sans\', sans-serif";
+    var labelFill = "#323232";
+    var bufferColor = "";
+    var bufferWidth = 0;
+    var textAlign = "left";
+    var offsetX = 0;
+    var offsetY = 0;
+    var placement = 'point';
+    if (feature.get("Atrac_Tur") !== null) {
+        labelText = String(feature.get("Atrac_Tur"));
+    }
+    var style = [ new ol.style.Style({
+        
+        fill: fill_BarrioEstacin_18,
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    }),new ol.style.Style({
+        
+        fill: fill_BarrioEstacin_18,
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    }),new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(0,0,0,0.2784313725490196)', lineDash: null, lineCap: 'square', lineJoin: 'bevel', width: 1.748}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+
+    return style;
+};
